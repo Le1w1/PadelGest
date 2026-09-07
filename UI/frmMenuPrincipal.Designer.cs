a486473d3c9c663cc111825fcc309a09362f0c76
Leonel Rodriguez <leorodriguezjesus@gmail.com>
2026-09-07 17:10:34 -0300

    .

--- UI/frmMenuPrincipal.Designer.cs
+++ UI/frmMenuPrincipal.Designer.cs
@@ -59,113 +59,92 @@ namespace UI
             // 
             // menuPrincipal
             // 
-            menuPrincipal.BackColor = Color.LightSkyBlue;
+            menuPrincipal.BackColor = Color.FromArgb(24, 70, 138);
+            menuPrincipal.ForeColor = Color.White;
             menuPrincipal.ImageScalingSize = new Size(20, 20);
             menuPrincipal.Items.AddRange(new ToolStripItem[] { mnuSesion, RecepcionistaToolStripMenuItem, vendedorBuffetToolStripMenuItem, encargadoDeCanchasToolStripMenuItem, dueñoToolStripMenuItem, mnuAdministrador });
             menuPrincipal.Location = new Point(0, 0);
             menuPrincipal.Name = "menuPrincipal";
-            // AirPadel style preview BEGIN
-            menuPrincipal.BackColor = Color.FromArgb(24, 70, 138);
-            menuPrincipal.ForeColor = Color.White;
-            // AirPadel style preview END
             menuPrincipal.Size = new Size(800, 24);
             menuPrincipal.TabIndex = 0;
             menuPrincipal.Text = "menuStrip1";
             // 
             // mnuSesion
             // 
-            mnuSesion.DropDownItems.AddRange(new ToolStripItem[] { reLoginToolStripMenuItem, cambiarClaveToolStripMenuItem, cambiarIdiomaToolStripMenuItem, cerrarSesionToolStripMenuItem });
-            mnuSesion.Name = "mnuSesion";
-            // AirPadel style preview BEGIN
             mnuSesion.BackColor = Color.FromArgb(24, 70, 138);
+            mnuSesion.DropDownItems.AddRange(new ToolStripItem[] { reLoginToolStripMenuItem, cambiarClaveToolStripMenuItem, cambiarIdiomaToolStripMenuItem, cerrarSesionToolStripMenuItem });
             mnuSesion.ForeColor = Color.White;
-            // AirPadel style preview END
+            mnuSesion.Name = "mnuSesion";
             mnuSesion.Size = new Size(59, 20);
             mnuSesion.Text = "Usuario";
             // 
             // reLoginToolStripMenuItem
             // 
-            reLoginToolStripMenuItem.Name = "reLoginToolStripMenuItem";
-            // AirPadel style preview BEGIN
             reLoginToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             reLoginToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            reLoginToolStripMenuItem.Name = "reLoginToolStripMenuItem";
             reLoginToolStripMenuItem.Size = new Size(159, 22);
             reLoginToolStripMenuItem.Text = "Re-Login";
             reLoginToolStripMenuItem.Click += reLoginToolStripMenuItem_Click;
             // 
             // cambiarClaveToolStripMenuItem
             // 
-            cambiarClaveToolStripMenuItem.Name = "cambiarClaveToolStripMenuItem";
-            // AirPadel style preview BEGIN
             cambiarClaveToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             cambiarClaveToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            cambiarClaveToolStripMenuItem.Name = "cambiarClaveToolStripMenuItem";
             cambiarClaveToolStripMenuItem.Size = new Size(159, 22);
             cambiarClaveToolStripMenuItem.Text = "Cambiar Clave";
             cambiarClaveToolStripMenuItem.Click += cambiarClaveToolStripMenuItem_Click;
             // 
             // cambiarIdiomaToolStripMenuItem
             // 
-            cambiarIdiomaToolStripMenuItem.Name = "cambiarIdiomaToolStripMenuItem";
-            // AirPadel style preview BEGIN
             cambiarIdiomaToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             cambiarIdiomaToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            cambiarIdiomaToolStripMenuItem.Name = "cambiarIdiomaToolStripMenuItem";
             cambiarIdiomaToolStripMenuItem.Size = new Size(159, 22);
             cambiarIdiomaToolStripMenuItem.Text = "Cambiar Idioma";
             cambiarIdiomaToolStripMenuItem.Click += cambiarIdiomaToolStripMenuItem_Click;
             // 
             // cerrarSesionToolStripMenuItem
             // 
-            cerrarSesionToolStripMenuItem.Name = "cerrarSesionToolStripMenuItem";
-            // AirPadel style preview BEGIN
             cerrarSesionToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             cerrarSesionToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            cerrarSesionToolStripMenuItem.Name = "cerrarSesionToolStripMenuItem";
             cerrarSesionToolStripMenuItem.Size = new Size(159, 22);
             cerrarSesionToolStripMenuItem.Text = "Cerrar Sesion";
             cerrarSesionToolStripMenuItem.Click += cerrarSesionToolStripMenuItem_Click;
             // 
             // RecepcionistaToolStripMenuItem
             // 
-            RecepcionistaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { seleccionarTurnoToolStripMenuItem });
-            RecepcionistaToolStripMenuItem.Name = "RecepcionistaToolStripMenuItem";
-            // AirPadel style preview BEGIN
             RecepcionistaToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
+            RecepcionistaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { seleccionarTurnoToolStripMenuItem });
             RecepcionistaToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            RecepcionistaToolStripMenuItem.Name = "RecepcionistaToolStripMenuItem";
             RecepcionistaToolStripMenuItem.Size = new Size(92, 20);
             RecepcionistaToolStripMenuItem.Text = "Recepcionista";
             // 
             // seleccionarTurnoToolStripMenuItem
             // 
-            seleccionarTurnoToolStripMenuItem.Name = "seleccionarTurnoToolStripMenuItem";
-            // AirPadel style preview BEGIN
             seleccionarTurnoToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             seleccionarTurnoToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            seleccionarTurnoToolStripMenuItem.Name = "seleccionarTurnoToolStripMenuItem";
             seleccionarTurnoToolStripMenuItem.Size = new Size(180, 22);
-            seleccionarTurnoToolStripMenuItem.Text = "Seleccionar Turno";
+            seleccionarTurnoToolStripMenuItem.Text = "Reservar Cancha";
             seleccionarTurnoToolStripMenuItem.Click += seleccionarTurnoToolStripMenuItem_Click;
             // 
             // vendedorBuffetToolStripMenuItem
             // 
-            vendedorBuffetToolStripMenuItem.Name = "vendedorBuffetToolStripMenuItem";
-            // AirPadel style preview BEGIN
             vendedorBuffetToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             vendedorBuffetToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            vendedorBuffetToolStripMenuItem.Name = "vendedorBuffetToolStripMenuItem";
             vendedorBuffetToolStripMenuItem.Size = new Size(104, 20);
             vendedorBuffetToolStripMenuItem.Text = "Vendedor Buffet";
             // 
             // encargadoDeCanchasToolStripMenuItem
             // 
-            encargadoDeCanchasToolStripMenuItem.Name = "encargadoDeCanchasToolStripMenuItem";
-            // AirPadel style preview BEGIN
             encargadoDeCanchasToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             encargadoDeCanchasToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            encargadoDeCanchasToolStripMenuItem.Name = "encargadoDeCanchasToolStripMenuItem";
             encargadoDeCanchasToolStripMenuItem.Size = new Size(139, 20);
             encargadoDeCanchasToolStripMenuItem.Text = "Encargado de Canchas";
             // 
@@ -177,68 +156,57 @@ namespace UI
             // 
             // mnuAdministrador
             // 
-            mnuAdministrador.DropDownItems.AddRange(new ToolStripItem[] { usuariosToolStripMenuItem, bitacoraEventosToolStripMenuItem, gestionarPerfilToolStripMenuItem, gestionDeRespaldoToolStripMenuItem });
-            mnuAdministrador.Name = "mnuAdministrador";
-            // AirPadel style preview BEGIN
             mnuAdministrador.BackColor = Color.FromArgb(24, 70, 138);
+            mnuAdministrador.DropDownItems.AddRange(new ToolStripItem[] { usuariosToolStripMenuItem, bitacoraEventosToolStripMenuItem, gestionarPerfilToolStripMenuItem, gestionDeRespaldoToolStripMenuItem });
             mnuAdministrador.ForeColor = Color.White;
-            // AirPadel style preview END
+            mnuAdministrador.Name = "mnuAdministrador";
             mnuAdministrador.Size = new Size(95, 20);
             mnuAdministrador.Text = "Administrador";
             // 
             // usuariosToolStripMenuItem
             // 
-            usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
-            // AirPadel style preview BEGIN
             usuariosToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             usuariosToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
             usuariosToolStripMenuItem.Size = new Size(219, 22);
             usuariosToolStripMenuItem.Text = "Gestionar Usuarios";
             usuariosToolStripMenuItem.Click += usuariosToolStripMenuItem_Click;
             // 
             // bitacoraEventosToolStripMenuItem
             // 
-            bitacoraEventosToolStripMenuItem.Name = "bitacoraEventosToolStripMenuItem";
-            // AirPadel style preview BEGIN
             bitacoraEventosToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             bitacoraEventosToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            bitacoraEventosToolStripMenuItem.Name = "bitacoraEventosToolStripMenuItem";
             bitacoraEventosToolStripMenuItem.Size = new Size(219, 22);
             bitacoraEventosToolStripMenuItem.Text = "Auditar Bitacora de Eventos";
             bitacoraEventosToolStripMenuItem.Click += bitacoraEventosToolStripMenuItem_Click;
             // 
             // gestionarPerfilToolStripMenuItem
             // 
-            gestionarPerfilToolStripMenuItem.Name = "gestionarPerfilToolStripMenuItem";
-            // AirPadel style preview BEGIN
             gestionarPerfilToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             gestionarPerfilToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            gestionarPerfilToolStripMenuItem.Name = "gestionarPerfilToolStripMenuItem";
             gestionarPerfilToolStripMenuItem.Size = new Size(219, 22);
             gestionarPerfilToolStripMenuItem.Text = "Gestion de Roles y Familias";
             gestionarPerfilToolStripMenuItem.Click += gestionarPerfilToolStripMenuItem_Click;
             // 
             // gestionDeRespaldoToolStripMenuItem
             // 
-            gestionDeRespaldoToolStripMenuItem.Name = "gestionDeRespaldoToolStripMenuItem";
-            // AirPadel style preview BEGIN
             gestionDeRespaldoToolStripMenuItem.BackColor = Color.FromArgb(24, 70, 138);
             gestionDeRespaldoToolStripMenuItem.ForeColor = Color.White;
-            // AirPadel style preview END
+            gestionDeRespaldoToolStripMenuItem.Name = "gestionDeRespaldoToolStripMenuItem";
             gestionDeRespaldoToolStripMenuItem.Size = new Size(219, 22);
             gestionDeRespaldoToolStripMenuItem.Text = "Gestion de Respaldo";
             gestionDeRespaldoToolStripMenuItem.Click += gestionDeRespaldoToolStripMenuItem_Click;
+            // 
             // statusStrip1
             // 
+            statusStrip1.BackColor = Color.FromArgb(24, 70, 138);
+            statusStrip1.ForeColor = Color.White;
             statusStrip1.ImageScalingSize = new Size(20, 20);
             statusStrip1.Items.AddRange(new ToolStripItem[] { lblUsuarioSesion, lblEstadoSesion });
             statusStrip1.Location = new Point(0, 428);
             statusStrip1.Name = "statusStrip1";
-            // AirPadel style preview BEGIN
-            statusStrip1.BackColor = Color.FromArgb(24, 70, 138);
-            statusStrip1.ForeColor = Color.White;
-            // AirPadel style preview END
             statusStrip1.Size = new Size(800, 22);
             statusStrip1.SizingGrip = false;
             statusStrip1.TabIndex = 1;
@@ -246,20 +214,16 @@ namespace UI
             // 
             // lblUsuarioSesion
             // 
-            lblUsuarioSesion.Name = "lblUsuarioSesion";
-            // AirPadel style preview BEGIN
             lblUsuarioSesion.ForeColor = Color.White;
-            // AirPadel style preview END
+            lblUsuarioSesion.Name = "lblUsuarioSesion";
             lblUsuarioSesion.Size = new Size(50, 17);
             lblUsuarioSesion.Text = "Usuario:";
             lblUsuarioSesion.TextAlign = ContentAlignment.MiddleLeft;
             // 
             // lblEstadoSesion
             // 
-            lblEstadoSesion.Name = "lblEstadoSesion";
-            // AirPadel style preview BEGIN
             lblEstadoSesion.ForeColor = Color.White;
-            // AirPadel style preview END
+            lblEstadoSesion.Name = "lblEstadoSesion";
             lblEstadoSesion.Size = new Size(735, 17);
             lblEstadoSesion.Spring = true;
             lblEstadoSesion.Text = "Sesión activa";
@@ -267,19 +231,17 @@ namespace UI
             // 
             // pnlInicio
             // 
+            pnlInicio.BackColor = Color.FromArgb(24, 70, 138);
             pnlInicio.Controls.Add(tableLayoutPanel1);
             pnlInicio.Dock = DockStyle.Fill;
             pnlInicio.Location = new Point(0, 24);
             pnlInicio.Name = "pnlInicio";
-            // AirPadel style preview BEGIN
-            pnlInicio.BackColor = Color.FromArgb(24, 70, 138);
-            // AirPadel style preview END
             pnlInicio.Size = new Size(800, 404);
             pnlInicio.TabIndex = 2;
             // 
             // tableLayoutPanel1
             // 
-            tableLayoutPanel1.BackColor = Color.PowderBlue;
+            tableLayoutPanel1.BackColor = Color.FromArgb(24, 70, 138);
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             tableLayoutPanel1.Controls.Add(lblDescripcionInicio, 0, 2);
@@ -287,9 +249,6 @@ namespace UI
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            // AirPadel style preview BEGIN
-            tableLayoutPanel1.BackColor = Color.FromArgb(24, 70, 138);
-            // AirPadel style preview END
             tableLayoutPanel1.RowCount = 5;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 30F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 15F));
@@ -303,11 +262,9 @@ namespace UI
             // 
             lblDescripcionInicio.Dock = DockStyle.Fill;
             lblDescripcionInicio.Font = new Font("Segoe UI", 11F);
+            lblDescripcionInicio.ForeColor = Color.White;
             lblDescripcionInicio.Location = new Point(3, 181);
             lblDescripcionInicio.Name = "lblDescripcionInicio";
-            // AirPadel style preview BEGIN
-            lblDescripcionInicio.ForeColor = Color.White;
-            // AirPadel style preview END
             lblDescripcionInicio.Size = new Size(794, 32);
             lblDescripcionInicio.TabIndex = 1;
             lblDescripcionInicio.Text = "Seleccione un módulo desde el menú superior para comenzar.";
@@ -317,11 +274,9 @@ namespace UI
             // 
             lblTituloInicio.Dock = DockStyle.Fill;
             lblTituloInicio.Font = new Font("Segoe UI", 22F);
+            lblTituloInicio.ForeColor = Color.White;
             lblTituloInicio.Location = new Point(3, 121);
             lblTituloInicio.Name = "lblTituloInicio";
-            // AirPadel style preview BEGIN
-            lblTituloInicio.ForeColor = Color.White;
-            // AirPadel style preview END
             lblTituloInicio.Size = new Size(794, 60);
             lblTituloInicio.TabIndex = 0;
             lblTituloInicio.Text = "Bienvenido a Padelgest";
@@ -329,21 +284,19 @@ namespace UI
             // 
             // frmMenuPrincipal
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(14, 43, 92);
             ClientSize = new Size(800, 450);
             ControlBox = false;
             Controls.Add(pnlInicio);
             Controls.Add(statusStrip1);
             Controls.Add(menuPrincipal);
+            Font = new Font("Segoe UI", 9.5F);
+            ForeColor = Color.White;
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MainMenuStrip = menuPrincipal;
             Name = "frmMenuPrincipal";
-            // AirPadel style preview BEGIN
-            BackColor = Color.FromArgb(14, 43, 92);
-            ForeColor = Color.White;
-            Font = new Font("Segoe UI", 9.5F);
-            // AirPadel style preview END
             StartPosition = FormStartPosition.CenterScreen;
             Text = "PadelGest - Menú Principal";
             WindowState = FormWindowState.Maximized;
